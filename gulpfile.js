8de4ea5ffd4c7820e382d3f6d1eaff5887f114df
Jason Ames <Chamberja_6@hotmail.com>
2016-01-16 22:23:16 +0000

    .

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,17 @@ var gulp = require('gulp'),
 	autoprefixer = require('gulp-autoprefixer'),
 	jshint = require('gulp-jshint'),
 	uglify = require('gulp-uglify'),
-	concat = require('gulp-require'),
+	concat = require('gulp-concat'),
 	notify = require('gulp-notify'),
 	cache = require('gulp-cache'),
 	rename = require('gulp-rename'),
 	del = require('del');
 
 gulp.task('styles', function() {
-  return sass('styles/main.scss', {style: expanded})
-
+	gulp.src('styles/*.scss')
   .pipe(autoprefixer())
   .pipe(gulp.dest('dist'))
   .pipe(rename({suffex: '.min'}))
-  .pipe(cssnano())
   .pipe(gulp.dest('dist'))
   .pipe(notify({message: 'Styles Task Complete'}));
 });
