9ad63ed2c01adaba869cd9d73216b352fba4ae61
Jason Ames <Chamberja_6@hotmail.com>
2016-01-16 22:52:26 +0000

    fml

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,11 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
 	concat = require('gulp-concat'),
 	notify = require('gulp-notify'),
-	cache = require('gulp-cache'),
 	rename = require('gulp-rename'),
 	del = require('del');
 
 gulp.task('styles', function() {
 	gulp.src('styles/*.scss')
-  .pipe(autoprefixer())
-  .pipe(gulp.dest('dist'))
-  .pipe(rename({suffex: '.min'}))
-  .pipe(gulp.dest('dist'))
-  .pipe(notify({message: 'Styles Task Complete'}));
+  .pipe(sass())
+  .pipe(gulp.dest('dist'));
 });
