d4e72929eba38b01ff9d783ae0f36acb79375785
Jason Ames <Chamberja_6@hotmail.com>
2016-01-16 22:05:26 +0000

    Changed some Modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,11 @@
 var gulp = require('gulp'),
 	sass = require('gulp-sass').
 	autoprefixer = require('gulp-autoprefixer'),
-	cssnano = require('gulp-cssnano'),
 	jshint = require('gulp-jshint'),
 	uglify = require('gulp-uglify'),
 	concat = require('gulp-require'),
 	notify = require('gulp-notify'),
 	cache = require('gulp-cache'),
-	livereload = require('gulp-livereload'),
 	rename = require('gulp-rename'),
 	del = require('del');
 
