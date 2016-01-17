b5a4912ff60f47eaa2cbd0ccaeae6c85f8ef4025
Jason Ames <Chamberja_6@hotmail.com>
2016-01-17 23:15:10 +0000

    Gulp Working

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,10 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass').
-	autoprefixer = require('gulp-autoprefixer'),
-	jshint = require('gulp-jshint'),
-	uglify = require('gulp-uglify'),
-	concat = require('gulp-concat'),
-	notify = require('gulp-notify'),
-	rename = require('gulp-rename'),
-	del = require('del');
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function() {
-	gulp.src('styles/*.scss')
+  gulp.src('styles/*.scss')
   .pipe(sass())
+  .pipe(autoprefixer('last 2 versions'))
   .pipe(gulp.dest('dist'));
 });
