7190ad06999d6a48f9256d603d5443292476f56e
Jason Ames <Chamberja_6@hotmail.com>
2016-01-20 20:22:36 +0000

    Added watch, browsersync and minify css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,32 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	autoprefixer = require('gulp-autoprefixer');
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer');
+  browserSync = require('browser-sync').create();
+  notify = require("gulp-notify");
+  plumber = require('gulp-plumber');
+  cssnano = require('gulp-cssnano');
+  rename = require("gulp-rename");
+
+gulp.task('serve', ['styles'], function() {
+
+  browserSync.init({
+   server: './dist'      
+  });
+
+  gulp.watch('styles/*.scss', ['styles']);
+  gulp.watch('dist/*.html').on('change', browserSync.reload);
+});
 
 gulp.task('styles', function() {
   gulp.src('styles/*.scss')
-  .pipe(sass())
-  .pipe(autoprefixer('last 2 versions'))
-  .pipe(gulp.dest('dist'));
+    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(cssnano())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream())
+    .pipe(notify('Build Finished'));
 });
+
+gulp.task('default', ['serve']);
