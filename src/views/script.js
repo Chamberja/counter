2f4cdd96f1b7fed8404206da0611ce2f9efadc6a
Jason Ames <jasonlrames@gmail.com>
2017-05-04 16:42:35 +0100

    wrhghw

--- src/views/script.js
+++ src/views/script.js
@@ -1,13 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const container = document.querySelector('.container');
-
-  const counter = container.querySelector('.counter');
-  const up = container.querySelector('.up');
-  const down = container.querySelector('.down');
   let current = 0;
 
   const render = (current) => {
-    counter.innerHTML = current ;
+    document.querySelector('.counter').innerHTML = current ;
   };
 
   document.addEventListener('click', (e)=> {
